3bfcec7fbe0de97cd59e4102bb7fa728472d006b
aktus <aktus@aktus-Inspiron-N5110.(none)>
2012-03-02 00:29:17 +0100

    initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aktus::Application.config.secret_token = '086884165059ae91f502d8d92aea5273f2fb19ad73b1d7e6a11ae63860d36e82d8348a5b881a91406b75ec00d89af13e1db5164995b867e45a58cf8cc74a7114'
